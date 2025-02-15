5acad86bda26e1595f68e1d028ec7cd515a3a7ed
Vlad Avramenko <vladavramenko213@gmail.com>
2025-02-15 20:45:52 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,85 @@
+{
+
+  const tasks = [];
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+    render();
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+  };
+
+  const bindEvents = () => {
+    const removeButtons = document.querySelectorAll(".js-remove");
+    console.log(removeButtons);
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index);
+      });
+    });
+
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+    console.log(removeButtons);
+
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+      });
+    });
+  }
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+                  <li class="list__item ${task.done ? "list__item--done" : ""}">
+                  <button class="js-done list__checkbox">${task.done ? "&#10004;" : ""}</button>
+                  <span class="list__text ${task.done ? "list__text--done" : ""}">${task.content}</span>
+                  <button class="js-remove list__deleteButton">&#128465;</button>
+                  </li>
+      `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    bindEvents();
+
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const taskInput = document.querySelector(".js-newTask");
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+    addNewTask(newTaskContent);
+    taskInput.value = "";
+    taskInput.focus();
+  };
+
+  const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+
+  };
+
+  init();
+}
