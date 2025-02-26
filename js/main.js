45cb8cd86cdef604c6fc971e0f622421bf5c2b9d
Vlad Avramenko <vladavramenko213@gmail.com>
2025-02-26 17:59:01 +0100

    split render function into separate renderTasks and render

--- js/main.js
+++ js/main.js
@@ -37,22 +37,38 @@
     });
   }
 
-  const render = () => {
+  const renderTasks = () => {
     let htmlString = "";
 
     for (const task of tasks) {
       htmlString += `
-                  <li class="list__item ${task.done ? "list__item--done" : ""}">
-                  <button class="js-done list__checkbox">${task.done ? "&#10004;" : ""}</button>
-                  <span class="list__text ${task.done ? "list__text--done" : ""}">${task.content}</span>
-                  <button class="js-remove list__deleteButton">&#128465;</button>
-                  </li>
+                <li class="list__item ${task.done ? "list__item--done" : ""}">
+                  <button class="js-done list__checkbox">
+                    ${task.done ? "&#10004;" : ""}
+                  </button>
+                  <span class="list__text ${task.done ? "list__text--done" : ""}">
+                    ${task.content}
+                  </span>
+                  <button class="js-remove list__deleteButton">
+                    &#128465;
+                  </button>
+                </li>
       `;
     }
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
+  };
 
+  const renderButtons = () => {};
+
+  const bindButtonsEvents = () => {};
+
+  const render = () => {
+    renderTasks();    
+    renderButtons();
+    
     bindEvents();
+    bindButtonsEvents();
 
   };
 
